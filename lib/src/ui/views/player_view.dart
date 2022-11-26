1f6e257253fd0e9ce77f08e37c1b943fcdd2d4a5
Aidan Juma <aidan@aidanjuma.dev>
2022-11-26 17:09:31 +0000

    🥳 included player with chewie now plays video

--- lib/src/ui/views/player_view.dart
+++ lib/src/ui/views/player_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+import 'package:provider/provider.dart';
+import 'package:video_player/video_player.dart';
+import 'package:tengoku/src/models/source.dart';
 import 'package:tengoku/src/mixins/orientation_mixins.dart';
-
-// TODO: Video Player with Chewie for anime.
+import 'package:tengoku/src/providers/consumet_provider.dart';
 
 class PlayerView extends StatefulWidget {
-  const PlayerView({super.key});
+  final String episodeId;
+  const PlayerView({super.key, required this.episodeId});
 
   @override
   State<PlayerView> createState() => _PlayerViewState();
@@ -12,14 +16,72 @@ class PlayerView extends StatefulWidget {
 
 class _PlayerViewState extends State<PlayerView>
     with ForceLandscapeStatefulModeMixin<PlayerView> {
+  String? src;
+  VideoPlayerController? _videoPlayerController;
+  ChewieController? _chewieController;
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return WillPopScope(
       onWillPop: () async => false,
-      child: SafeArea(
-        child: Scaffold(),
+      child: Consumer<ConsumetProvider>(
+        builder: ((context, provider, child) {
+          if (_chewieController != null) {
+            return SafeArea(
+              child: Scaffold(
+                backgroundColor: Colors.black,
+                body: Chewie(controller: _chewieController!),
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(color: Colors.white),
+          );
+        }),
       ),
     );
+  }
+
+  Future<void> initPlayer() async {
+    final ConsumetProvider provider =
+        Provider.of<ConsumetProvider>(context, listen: false);
+
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) async {
+        await provider.getStreamingLinksFromEpisodeId(widget.episodeId);
+        // When source fetched; if not null, find and use default quality URL.
+        if (provider.currentAnimeSource != null) {
+          final Source source = provider.currentAnimeSource!;
+          src = source.videos
+              .where((video) => video.quality == "default")
+              .first
+              .url;
+          // Video Player & Chewie: Initialization.
+          _videoPlayerController = VideoPlayerController.network(src!);
+          _videoPlayerController!.initialize();
+
+          _chewieController = ChewieController(
+            videoPlayerController: _videoPlayerController!,
+            aspectRatio: 16 / 9,
+            fullScreenByDefault: true,
+            // TODO: Error Builder, Custom Controls...
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController?.dispose();
+    _chewieController?.dispose();
+    super.dispose();
   }
 }
