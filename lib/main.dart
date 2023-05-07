6d7203c67386ec152c669d48e92d5c49aeebdb46
Aidan Juma <aidan@aidanjuma.dev>
2023-05-07 16:53:42 +0100

    ✅ fixed Isar database directory issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:provider/provider.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:tengoku/src/ui/theme/global.dart';
 import 'package:tengoku/src/models/anime_result.dart';
 import 'package:tengoku/src/models/anime_episode.dart';
@@ -9,9 +10,16 @@ import 'package:tengoku/src/router/navigator_wrapper.dart';
 import 'package:tengoku/src/providers/consumet_provider.dart';
 import 'package:tengoku/src/mixins/force_portrait_mode_mixin.dart';
 
+import 'dart:io';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // Open Isar instance at app start time.
-  final Isar isar = await Isar.open([AnimeEpisodeSchema, AnimeResultSchema]);
+  final Directory dir = await getApplicationDocumentsDirectory();
+  final Isar isar = await Isar.open(
+    [AnimeEpisodeSchema, AnimeResultSchema],
+    directory: dir.path,
+  );
   final app = Application(isar: isar);
   runApp(app);
 }
--- lib/src/models/anime_result.g.dart
+++ lib/src/models/anime_result.g.dart
@@ -7,7 +7,7 @@ part of 'anime_result.dart';
 // **************************************************************************
 
 // coverage:ignore-file
-// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters, always_specify_types
 
 extension GetAnimeResultCollection on Isar {
   IsarCollection<AnimeResult> get animeResults => this.collection();
@@ -77,7 +77,7 @@ const AnimeResultSchema = CollectionSchema(
   getId: _animeResultGetId,
   getLinks: _animeResultGetLinks,
   attach: _animeResultAttach,
-  version: '3.0.5',
+  version: '3.1.0+1',
 );
 
 int _animeResultEstimateSize(
